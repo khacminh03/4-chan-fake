e6c23ebc44d9af7a64bff3dd14093d5ffe69c03a
D4rULi3b3rt <122429060+khacminh03@users.noreply.github.com>
2023-10-25 16:07:21 +0700

    Update register.php

--- register.php
+++ register.php
@@ -2,9 +2,26 @@
     session_start();
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $username = $_POST['username'];
-        $password = $_POST['password'];
-        echo $username;
-        echo $password;
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $conn = mysqli_connect("localhost", "root", "", "forum");
+        if ($conn->connect_error) {
+            die ("connection failed: " . $conn->connect_error);
+        }
+        $unique = "SELECT * FROM users WHERE username = '$username'";
+        $result = $conn->query($unique);
+        if ($result->num_rows > 0) {
+            echo "<script>alert('Username has been taken')</script>";
+            echo "<script>window.location = 'register.html'</script>";
+            exit();
+        }
+        $sql = "INSERT INTO users (username, password, role) VALUES ('$username', '$password', 'normal')";
+        if ($conn->query($sql) === true) {
+            echo "<script>alert('Register successfully')</script>";
+            echo "<script>window.location = 'index.html';</script>";
+        } else {
+            echo "Đéo ổn rồi!";
+        }
+        $conn->close();
     }
     
 ?>
